52758ae3bd683fd436f384ed01e18056d6c96616
anowakowski <nowakowski.add@gmail.com>
2018-12-12 13:56:51 +0100

    create functionality to show/hide menu

--- angularmaterial/src/app/contactmanager/components/sidenav/sidenav.component.ts
+++ angularmaterial/src/app/contactmanager/components/sidenav/sidenav.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+
+const SMALL_WIDTH_BREAKPOINT = 720;
 
 @Component({
   selector: 'app-sidenav',
@@ -7,11 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidenavComponent implements OnInit {
 
-  constructor() { }
+  private mediaMatcher: MediaQueryList =
+    matchMedia(`(max-width: ${SMALL_WIDTH_BREAKPOINT}px)`);
+
+    constructor(
+      zone: NgZone,
+      private router: Router) {
+      // this.mediaMatcher.addListener(mql =>
+      //   zone.run(() => this.mediaMatcher = mql));
+    }
 
   showFiller = false;
 
   ngOnInit() {
+  }
+
+  isScreenSmall(): boolean {
+    return this.mediaMatcher.matches;
   }
 
 }
