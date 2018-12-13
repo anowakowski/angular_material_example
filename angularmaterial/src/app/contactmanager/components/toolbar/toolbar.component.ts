8d5fa26f9c36113e78ac70e099777878318b9a40
anowakowski <nowakowski.add@gmail.com>
2018-12-13 14:23:27 +0100

    prepare new contact dialog with vlidation reactive form

--- angularmaterial/src/app/contactmanager/components/toolbar/toolbar.component.ts
+++ angularmaterial/src/app/contactmanager/components/toolbar/toolbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import {MatDialog} from '@angular/material';
+import { NewContactDialogComponent } from '../new-contact-dialog/new-contact-dialog.component';
 
 @Component({
   selector: 'app-toolbar',
@@ -9,13 +11,24 @@ export class ToolbarComponent implements OnInit {
 
   @Output() toggleSidenav = new EventEmitter<void>();
 
-  constructor() { }
+
+  constructor(private dialog: MatDialog) { }
 
   ngOnInit() {
   }
 
   emitMenu() {
     this.toggleSidenav.emit();
+  }
+
+  openAddContactDialog(): void {
+    const dialogRef = this.dialog.open(NewContactDialogComponent, {
+      width: '450px'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed', result);
+    });
   }
 
 }
